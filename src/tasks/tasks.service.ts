1de7da3abf86c8e74d5f3fd75f951ec71aa60382
shj <josh2kv@gmail.com>
2023-02-04 23:35:09 +0900

    Feature Get Task by ID

--- src/tasks/tasks.service.ts
+++ src/tasks/tasks.service.ts
@@ -11,6 +11,12 @@ export class TasksService {
     return this.tasks;
   }
 
+  getTask(id: string): Task {
+    const task = this.tasks.find((task) => task.id === id);
+
+    return task;
+  }
+
   createTask(createTaskDto: CreateTaskDto): Task {
     const { title, description } = createTaskDto;
 
