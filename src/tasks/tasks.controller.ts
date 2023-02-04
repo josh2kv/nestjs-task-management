7e1d9557c3f7c896dfbfbbb370badecbaf4f3f93
shj <josh2kv@gmail.com>
2023-02-05 01:07:58 +0900

    Validation: Update Task Status

--- src/tasks/tasks.controller.ts
+++ src/tasks/tasks.controller.ts
@@ -12,6 +12,7 @@ import {
 import { TasksService } from './tasks.service';
 import { Task, TaskStatus } from './task.model';
 import { CreateTaskDto } from './dto/create-task.dto';
+import { UpdateTaskStatusDto } from './dto/update-task-status.dto';
 
 @Controller('tasks')
 export class TasksController {
@@ -42,8 +43,9 @@ export class TasksController {
   @Patch(':id/status')
   updateTaskStatus(
     @Param('id') id: string,
-    @Body('status') status: TaskStatus,
+    @Body() updateTaskStatus: UpdateTaskStatusDto,
   ) {
+    const { status } = updateTaskStatus;
     return this.tasksService.updateTask(id, status);
   }
 }
