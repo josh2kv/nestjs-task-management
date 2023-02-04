4bc695a96c1c364e61c78ee0753a947fe17d796e
shj <josh2kv@gmail.com>
2023-02-04 21:30:42 +0900

    Feature: Get all Tasks

--- src/tasks/tasks.controller.ts
+++ src/tasks/tasks.controller.ts
@@ -1,7 +1,12 @@
+import { Controller, Get } from '@nestjs/common';
 import { TasksService } from './tasks.service';
-import { Controller } from '@nestjs/common';
 
 @Controller('tasks')
 export class TasksController {
   constructor(private tasksService: TasksService) {}
+
+  @Get()
+  getAllTasks() {
+    return this.tasksService.getAllTasks();
+  }
 }
