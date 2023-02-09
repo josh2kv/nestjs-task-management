7366c10f8dd578acc204aa142910d1541c64e673
shj <josh2kv@gmail.com>
2023-02-09 17:31:32 +0900

    Install JWTModule and Passport with NestJS

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -1,12 +1,23 @@
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { Module } from '@nestjs/common';
+import { PassportModule } from '@nestjs/passport';
+import { JwtModule } from '@nestjs/jwt';
+import { AuthController } from './auth.controller';
 import { AuthService } from './auth.service';
 import { UsersService } from './users.service';
 import { User } from './user.entity';
-import { AuthController } from './auth.controller';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([User])],
+  imports: [
+    TypeOrmModule.forFeature([User]),
+    PassportModule.register({ defaultStrategy: 'jwt' }),
+    JwtModule.register({
+      secret: 'topSecret51',
+      signOptions: {
+        expiresIn: 3600,
+      },
+    }),
+  ],
   providers: [AuthService, UsersService],
   controllers: [AuthController],
 })
