19d251617114bd738497777f428179e4cc3fd839
shj <josh2kv@gmail.com>
2023-03-19 23:26:34 +0900

    fix: getTasks query

--- src/tasks/tasks.service.ts
+++ src/tasks/tasks.service.ts
@@ -29,7 +29,7 @@ export class TasksService {
 
     if (search)
       query.andWhere(
-        'LOWER(task.title) LIKE LOWER(:search) OR LOWER(task.description) LIKE LOWER(:search)',
+        '(LOWER(task.title) LIKE LOWER(:search) OR LOWER(task.description) LIKE LOWER(:search))',
         { search: `%${search}%` },
       );
 
